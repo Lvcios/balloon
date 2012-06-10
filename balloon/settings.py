caad9483e8d7115cc1cbddf4370518b060d421f4
Lucio Flores <lvcios.informatico@gmail.com>
2012-06-09 22:58:42 -0500

    templates mexico says

--- balloon/settings.py
+++ balloon/settings.py
@@ -121,7 +121,8 @@ ROOT_URLCONF = 'balloon.urls'
 WSGI_APPLICATION = 'balloon.wsgi.application'
 
 TEMPLATE_DIRS = (
-	'/var/www/html/balloon/template',
+	#'/var/www/html/balloon/template',
+	'/app/template/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
