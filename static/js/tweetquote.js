755fc340bffedcc7a20d31bfc3ed90e876e4b356
Lucio Flores <lvcios.informatico@gmail.com>
2012-06-10 13:18:48 -0500

    add static files

--- static/js/tweetquote.js
+++ static/js/tweetquote.js
@@ -163,13 +163,13 @@ var tweetquote = function () {
 			text = text.replace( pattern_tags , '');
 		}
 		
-		var html = '';
+		//var html = '<h1><span id="' + textId + '">' + text + '</span></h1>';
 		
 		if( config.includeauthor ) {
-			html += '<h3><span id="' + authorId + '">' + '<a href="http://www.twitter.com/' + tweet.from_user + '">' + tweet.from_user + '</a><h4>Dijo</h4></span></h3>';
+			var html = '<h3><span id="' + authorId + '">' + '<a href="http://www.twitter.com/' + tweet.from_user + '">' + tweet.from_user + '</a><h4>Dijo</h4></span></h3>';
 		}
 		
-		html = '<h1><span id="' + textId + '">' + text + '</span></h1>';
+		html+ = '<h1><span id="' + textId + '">' + text + '</span></h1>';
 				
 		if( handlerTextParser ) {
 			return( handlerTextParser( html, tweet ) );
