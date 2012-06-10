99f1c0cb9144be1a25bdb638e95b8a4abb75a1f7
Lucio Flores <lvcios.informatico@gmail.com>
2012-06-10 13:22:10 -0500

    add static files

--- static/js/tweetquote.js
+++ static/js/tweetquote.js
@@ -166,7 +166,7 @@ var tweetquote = function () {
 		var html = '<h1><span id="' + textId + '">' + text + '</span></h1>';
 		
 		if( config.includeauthor ) {
-			var html = '<h3><span id="' + authorId + '">' + '<a href="http://www.twitter.com/' + tweet.from_user + '">' + tweet.from_user + '</a><h4>Dijo</h4></span></h3>';
+			html += '<h4>fue lo que Dijo :</h4><<h3><span id="' + authorId + '">' + '<a href="http://www.twitter.com/' + tweet.from_user + '">' + tweet.from_user + '</a>/span></h3>';
 		}
 		
 		
